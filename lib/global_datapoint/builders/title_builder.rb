7819f25f5985aa8823b328e9a9f7ee67820a3198
Richard Patching <richard@justaddpixels.com>
2012-12-04 12:54:02 +0000

    Build a title

--- lib/global_datapoint/builders/title_builder.rb
+++ lib/global_datapoint/builders/title_builder.rb
@@ -13,8 +13,44 @@ module GlobalDatapoint
         @xml.search('title_id').first.children.text
       end
 
+      def created_date
+        date_for(@xml.search('created_date').first.children.text)
+      end
+
+      def modified_date
+        date_for(@xml.search('modified_date').first.children.text)
+      end
+
+      def date_for(date)
+        Date.strptime(date, '%d/%m/%Y')
+      end
+
+      def name
+        @xml.search('title_name').first.children.text
+      end
+
+      def description
+        @xml.search('title_description').first.children.text
+      end
+
+      def type
+        @xml.search('title_type').first.children.text
+      end
+
+      def short_description
+        @xml.search('title_shortdescription').first.children.text
+      end
+
       def build
-        Title.new(:title_id => title_id)
+        Title.new(
+          :title_id => title_id,
+          :created_date => created_date,
+          :modified_date => modified_date,
+          :name => name,
+          :description => description,
+          :type => type,
+          :short_description => short_description
+        )
       end
     end
   end

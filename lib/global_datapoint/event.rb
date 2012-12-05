3fde189f3a5cc0d62a4085d0c4866f6d0b5e6f31
Richard Patching <richard@justaddpixels.com>
2012-12-05 14:40:36 +0000

    Add details to event

--- lib/global_datapoint/event.rb
+++ lib/global_datapoint/event.rb
@@ -9,7 +9,39 @@ module GlobalDatapoint
     end
 
     def event_id
-      @options.fetch(:event_id, nil)
+      attribute_for(:event_id)
+    end
+
+    def start_date
+      attribute_for(:start_date)
+    end
+
+    def end_date
+      attribute_for(:end_date)
+    end
+
+    def start_time
+      attribute_for(:start_time)
+    end
+
+    def end_time
+      attribute_for(:end_time)
+    end
+
+    def note
+      attribute_for(:note)
+    end
+
+    def booking_link
+      attribute_for(:booking_link)
+    end
+
+    def ticketmaster_link
+      attribute_for(:ticketmaster_link)
+    end
+
+    def attribute_for(attr)
+      @options.fetch(attr, nil)
     end
   end
 end

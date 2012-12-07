b11e98cab610fd0fd70b8ad54fa3f210683d42b1
Richard Patching <richard@justaddpixels.com>
2012-12-07 19:19:36 +0000

    Add performance to event

--- lib/global_datapoint/event.rb
+++ lib/global_datapoint/event.rb
@@ -40,6 +40,12 @@ module GlobalDatapoint
       attribute_for(:ticketmaster_link)
     end
 
+    def performance
+      attribute_for(:performance)
+    end
+
+    private
+
     def attribute_for(attr)
       @options.fetch(attr, nil)
     end
--- lib/global_datapoint/version.rb
+++ lib/global_datapoint/version.rb
@@ -1,3 +1,3 @@
 module GlobalDatapoint
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
